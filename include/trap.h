c8661adcbbe1e6a88ee6543de4a353a1a3215fb8
Timothy M. Schaeffer <tschaeffer@dramail.com>
2009-12-12 17:53:19 -0500

    Fix bogus cpp conditional in header.

--- include/trap.h
+++ include/trap.h
@@ -22,7 +22,7 @@
 #elif defined(AT90S8515)
 #include "avr/at90s8515/trap.h"
 #elif defined(ATMEGA644P)
-#elif "avr/atmega644p/trap.h"
+#include "avr/atmega644p/trap.h"
 #elif defined(I386)
 #include "i386/trap.h"
 #else
